994ef9e8553acd24c11d9badc59c3930fa6fd163
dikshasa17 <sainidiksha2404@gmail.com>
2022-09-04 21:32:47 +0530

    google sign in enabled

--- whatsappclone/src/Login.js
+++ whatsappclone/src/Login.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Login.css'
 import { Button } from '@mui/material'
+import { auth, provider } from './firebase'
 function Login()
    {
     const signIn = () =>{
-        
-    }
+        auth.signInWithPopup(provider)
+        .then((result) => console.log(result))
+        .catch((error) =>alert(error.message));
+    };
   return (
     <div className='login'>
         <div className='login_container'>
