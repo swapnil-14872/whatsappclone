a13257007bd7ba780a473ee9352775b7ebaaf1e9
dikshasa17 <sainidiksha2404@gmail.com>
2022-09-04 19:47:56 +0530

    sample login page added incase user not logged in

--- whatsappclone/src/App.js
+++ whatsappclone/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 function App() {
+	const [user,setuser]= useState(null);
+	{/*if there is a user show a login screen else show your chats*/}
   return (
     <div className="app">
-     <div className="app_body">
+		{!user ? (<h1>Login</h1>) :(<div className="app_body">
 	 <BrowserRouter>
 	 <Sidebar/> {/*always  show sidebar*/}
 			<Routes>
@@ -15,8 +17,8 @@ function App() {
 			</Routes>
 	 </BrowserRouter>
       
-      </div>
-    </div>
+      </div>)}
+	  </div>
   );
 }
 
